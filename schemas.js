2a85237f9323c81d3a4ddb47f8cb2cb36404bf00
SiyuLiu0 <527100746@qq.com>
2021-12-25 16:33:45 -0800

    Add review funtion

--- schemas.js
+++ schemas.js
@@ -1,5 +1,5 @@
-
 const Joi = require('joi');
+const { number } = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -10,3 +10,10 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
