9eddd8d201cae789986dc4ad75a608c1207752b3
SiyuLiu0 <527100746@qq.com>
2021-12-28 19:47:49 -0800

    add clustermap to the app

--- models/campground.js
+++ models/campground.js
@@ -10,10 +10,11 @@ const ImageSchema = new Schema({
     url: String,
     filename: String
 });
-
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
+
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -42,7 +43,15 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
 
 
 
