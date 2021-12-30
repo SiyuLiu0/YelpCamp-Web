641c95c1d5977a2b3a863566c36312b47482a92f
SiyuLiu0 <527100746@qq.com>
2021-12-29 18:29:27 -0800

    rename files agin

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const ExpressError = require('./utils/ExpressError');
+const ExpressError = require('./Utils/ErrorHandler');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
